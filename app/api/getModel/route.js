9371f7a5ab3d4ff511bb4dbecc86469cad68d226
valen07dido <valendido69@gmail.com>
2025-01-06 16:06:53 -0300

    deploy try 1

--- app/api/getModel/route.js
+++ app/api/getModel/route.js
@@ -4,31 +4,29 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   try {
     const url = new URL(request.url);
-    const page = parseInt(url.searchParams.get('page') || '1', 10);
-    const limit = parseInt(url.searchParams.get('limit') || '9', 10);
-    const categoryFilter = url.searchParams.get('category') || '';
-    const typeFilter = url.searchParams.get('type') || '';
-    
+    const page = Math.max(1, parseInt(url.searchParams.get("page") || "1", 10)); // Asegura que sea al menos 1
+    const limit = Math.max(1, parseInt(url.searchParams.get("limit") || "9", 10)); // Asegura que sea al menos 1
+    const categoryFilter = url.searchParams.get("category") || "";
+    const typeFilter = url.searchParams.get("type") || "";
+
     const offset = (page - 1) * limit;
 
-    // Asegúrate de que los nombres de las columnas sean correctos aquí
-    const query = sql`
+    // Query para los productos
+    const result = await sql`
       SELECT * FROM "Models"
       WHERE "categories" ILIKE ${`%${categoryFilter}%`} 
       AND "type" ILIKE ${`%${typeFilter}%`}
       LIMIT ${limit} OFFSET ${offset};
     `;
 
-    const result = await query;
-
-    // Para obtener el total de productos que coinciden con los filtros
-    const countQuery = sql`
+    // Query para el total de productos
+    const countResult = await sql`
       SELECT COUNT(*) FROM "Models"
       WHERE "categories" ILIKE ${`%${categoryFilter}%`} 
       AND "type" ILIKE ${`%${typeFilter}%`}
     `;
-    const countResult = await countQuery;
-    const totalItems = parseInt(countResult.rows[0].count, 9);
+
+    const totalItems = parseInt(countResult.rows[0].count, 10); // Base 10
     const totalPages = Math.ceil(totalItems / limit);
 
     const responseData = {
@@ -40,15 +38,14 @@ export async function GET(request) {
 
     const response = NextResponse.json(responseData, { status: 200 });
 
-    // Configura las cabeceras para la caché
+    // Configura las cabeceras para evitar caché
     response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
     response.headers.set("Pragma", "no-cache");
     response.headers.set("Expires", "0");
 
     return response;
-
   } catch (error) {
     console.error("Error en la consulta:", error);
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ error: "Error interno del servidor" }, { status: 500 });
   }
 }
---
+++ app/nosotros/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      sadasdasdadasd
+    </div>
+  )
+}
+
+export default page
